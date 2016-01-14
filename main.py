62fb83837b38584cb3b92b347c2ac71ca3261382
Qu Dong <qudong452@gmail.com>
2016-01-14 22:07:16 +0800

    HW2 Part2 finished
    Signed-off-by: Qu Dong <qudong452@gmail.com>

--- main.py
+++ main.py
@@ -17,6 +17,7 @@
 import webapp2
 import cgi
 import string
+import re
 
 form = """
 <form method="post">
@@ -172,9 +173,9 @@ class Rot13Page(webapp2.RequestHandler):
     def post(self):
         # uploaded_file = self.request.body
         # content = uploaded_file.split("=")[1]
-        content=self.request.get('text')
+        content = self.request.get('text')
 
-        self.fill_rot13html(escape_html(content.encode('rot13'))) # must to have the html escape
+        self.fill_rot13html(escape_html(content.encode('rot13')))  # must to have the html escape
         # self.fill_rot13html(content.encode('rot13'))
 
         # TEST http request contents
@@ -187,9 +188,137 @@ class Rot13Page(webapp2.RequestHandler):
         # self.response.out.write(rot13_transfer(content))
 
 
+signuphtml = """
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Sign Up</title>
+    <style type="text/css">
+      .label {text-align: right}
+      .error {color: red}
+    </style>
+  </head>
+
+  <body>
+    <h2>Signup</h2>
+    <form method="post">
+    <table>
+        <tr>
+            <td class=label>Username</td>
+            <td><input type="text" name="username" value=%(username)s></td>
+            <td class=error>%(name_err)s</td>
+        </tr>
+        <tr>
+            <td class=label>Password</td>
+            <td><input type="password" name="pw1"></td>
+            <td class=error>%(pw1_err)s</td>
+        </tr>
+        <tr>
+            <td class=label>Verify Password</td>
+            <td><input type="password" name="pw2"></td>
+            <td class=error>%(pw2_err)s</td>
+        </tr>
+        <tr>
+            <td class=label>Email (optional)</td>
+            <td><input type="text" name="email" value=%(email)s></td>
+            <td class=error>%(email_err)s</td>
+        </tr>
+    </table>
+      <input type="submit">
+    </form>
+  </body>
+
+</html>
+"""
+
+# todo: to understand how re works
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PW_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+
+
+def valid_username(username):
+    return USER_RE.match(username)
+
+
+def valid_password(password):
+    return PW_RE.match(password)
+
+
+def valid_email(email):
+    return EMAIL_RE.match(email)
+
+
+class SignupPage(webapp2.RequestHandler):
+    def fill_signup(self, user_str="", email_str="", name_err="", pw1_err="", pw2_err="", email_err=""):
+        self.response.out.write(signuphtml % {'username': user_str,
+                                              'email': email_str,
+                                              'name_err': name_err,
+                                              'pw1_err': pw1_err,
+                                              'pw2_err': pw2_err,
+                                              'email_err': email_err})
+
+    def get(self):
+        self.fill_signup()
+
+    def post(self):
+        user_username = self.request.get('username')
+        user_pw1 = self.request.get('pw1')
+        user_pw2 = self.request.get('pw2')
+        user_email = self.request.get('email')
+
+        username = valid_username(user_username)
+        pw1 = valid_password(user_pw1)
+        pw2 = valid_password(user_pw2)
+        pw_same = True if user_pw1 == user_pw2 else False
+        email = valid_email(user_email)
+
+        if not user_email:  # since it is optional
+            email = True
+
+        if not (username and pw1 and pw2 and email and pw_same):
+            if not username:
+                user_err = "That's not a valid username."
+            else:
+                user_err = ""
+            if not pw1:
+                pw1_err = "That wasn't a valid password."
+            else:
+                pw1_err = ""
+            if pw1 and not pw_same:
+                pw2_err = "Your passwords didn't match."
+            else:
+                pw2_err = ""
+            if not email:
+                email_err = "That's not a valid email."
+            else:
+                email_err = ""
+
+            self.fill_signup(user_str=user_username,
+                             email_str=user_email,
+                             name_err=user_err,
+                             pw1_err=pw1_err,
+                             pw2_err=pw2_err,
+                             email_err=email_err)
+
+        else:
+            # For redirect, default it is using GET, and GET pass params in the URL
+            self.redirect(
+                '/signup/welcome?username=' + user_username)  # NOTE: how to pass the infor through GET method during redirect
+
+
+class WelcomePage(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get('username')
+        self.response.out.write("Welcome, {}".format(username))
+
+
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/testform', TestPage),
                                ('/thanks', ThanksPage),
                                ('/rot13', Rot13Page),
+                               ('/signup', SignupPage),
+                               ('/signup/welcome', WelcomePage)
                                ],
                               debug=True)
